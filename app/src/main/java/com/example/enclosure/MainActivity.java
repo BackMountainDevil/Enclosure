ec70bb9112ef671b746a111a9d18942ad8d6fdfd
Kearney <kearneyyi@cau.edu.cn>
2020-09-03 16:41:30 +0800

    实现当前位置的蓝点显示

--- app/src/main/java/com/example/enclosure/MainActivity.java
+++ app/src/main/java/com/example/enclosure/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import com.amap.api.maps2d.AMap;
 import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.model.MyLocationStyle;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -29,6 +30,16 @@ public class MainActivity extends AppCompatActivity {
         }
         // 显示卫星地图模式,可选类型：MAP_TYPE_NORMAL,MAP_TYPE_SATELLITE,MAP_TYPE_NIGHT
         aMap.setMapType(AMap.MAP_TYPE_SATELLITE);
+
+
+        //定位蓝点，https://lbs.amap.com/api/android-sdk/guide/create-map/mylocation#location-marker-5-0
+        MyLocationStyle myLocationStyle;
+        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+//aMap.getUiSettings().setMyLocationButtonEnabled(true);设置默认定位按钮是否显示，非必需设置。
+        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+
     }
     @Override
     protected void onDestroy() {
