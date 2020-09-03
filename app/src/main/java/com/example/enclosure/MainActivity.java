23557788fb18ff70c2b0814dbffe1e679ecf11e7
Kearney <kearneyyi@cau.edu.cn>
2020-09-03 20:32:17 +0800

    实现地图上面添加按钮并有点击Toast提示

--- app/src/main/java/com/example/enclosure/MainActivity.java
+++ app/src/main/java/com/example/enclosure/MainActivity.java
@@ -3,44 +3,79 @@ package com.example.enclosure;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 import com.amap.api.maps2d.AMap;
 import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.UiSettings;
 import com.amap.api.maps2d.model.MyLocationStyle;
 
 
-public class MainActivity extends AppCompatActivity {
-    MapView mMapView = null;
-    AMap aMap = null;
+public class MainActivity extends AppCompatActivity  {
+    private MapView mMapView = null;
+    private AMap aMap = null;
+    private UiSettings mUiSettings;//定位按钮
+    private MyLocationStyle myLocationStyle;   //蓝点
 
+    private Button btn_click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("大师抄的地图");
         setContentView(R.layout.activity_main);
-        //获取地图控件引用
-        mMapView = findViewById(R.id.mapview);
-        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mMapView = findViewById(R.id.mapview);       //获取地图控件引用
         mMapView.onCreate(savedInstanceState);// 此方法须覆写，虚拟机需要在很多情况下保存地图绘制的当前状态。
+        init();
+    }
 
-        //初始化地图控制器对象
+
+    /**
+     * 初始化AMap对象 - 地图控制器
+     */
+    private void init() {
         if (aMap == null) {
-            aMap =  mMapView.getMap();
+            aMap = mMapView.getMap();
+            setUpMap();
         }
-        // 显示卫星地图模式,可选类型：MAP_TYPE_NORMAL,MAP_TYPE_SATELLITE,MAP_TYPE_NIGHT
-        aMap.setMapType(AMap.MAP_TYPE_SATELLITE);
 
-
-        //定位蓝点，https://lbs.amap.com/api/android-sdk/guide/create-map/mylocation#location-marker-5-0
-        MyLocationStyle myLocationStyle;
-        myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
-        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
-        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
-//aMap.getUiSettings().setMyLocationButtonEnabled(true);设置默认定位按钮是否显示，非必需设置。
-        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
 
     }
+
+    /**
+     * 设置一些aMap的属性,添加一些事件监听器
+     */
+    private void setUpMap() {
+        mUiSettings = aMap.getUiSettings();//实例化UiSettings类对象
+        aMap.setMapType(AMap.MAP_TYPE_SATELLITE);       //卫星地图模式
+
+        //定位按钮
+        mUiSettings.setMyLocationButtonEnabled(true); //显示默认的定位按钮
+        mUiSettings.setZoomControlsEnabled(false);  //取消显示默认的缩放按钮
+        aMap.setMyLocationEnabled(true);// 可触发定位并显示当前位置
+
+        btn_click = findViewById(R.id.btn_gps);
+
+        btn_click.setOnClickListener(new MyOnClickListener());
+    }
+
+    class MyOnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) { // 点击事件的处理方法
+            if (v.getId() == R.id.btn_gps) { // 判断是否为btn_click被点击
+                if(aMap.isMyLocationEnabled())
+                {
+                    double la = aMap.getMyLocation().getLatitude();
+                    double lo = aMap.getMyLocation().getLongitude();
+                    Toast.makeText(getApplicationContext(), " la：" +la+" lo: "+lo, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -65,5 +100,6 @@ public class MainActivity extends AppCompatActivity {
         //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
         mMapView.onSaveInstanceState(outState);
     }
+
 
 }
