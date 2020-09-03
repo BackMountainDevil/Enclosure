0e5e09558e85c013087c5574df5179d0f781d4e1
Kearney <kearneyyi@cau.edu.cn>
2020-09-03 16:34:06 +0800

    补全地图aMap的生命周期，将之前的几十个warning全部解决掉了

--- app/src/main/java/com/example/enclosure/MainActivity.java
+++ app/src/main/java/com/example/enclosure/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.enclosure;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,7 +9,8 @@ import com.amap.api.maps2d.MapView;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    MapView mMapView = null;
+    AMap aMap = null;
 
 
     @Override
@@ -16,14 +18,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTitle("大师抄的地图");
         setContentView(R.layout.activity_main);
+        //获取地图控件引用
+        mMapView = findViewById(R.id.mapview);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mMapView.onCreate(savedInstanceState);// 此方法须覆写，虚拟机需要在很多情况下保存地图绘制的当前状态。
 
-        MapView mapView = (MapView) findViewById(R.id.mapview);
-        mapView.onCreate(savedInstanceState);// 此方法必须重写
-        AMap aMap = mapView.getMap();
-
-        aMap.setTrafficEnabled(true);// 显示实时交通状况
-        //地图模式可选类型：MAP_TYPE_NORMAL,MAP_TYPE_SATELLITE,MAP_TYPE_NIGHT
-        aMap.setMapType(AMap.MAP_TYPE_SATELLITE);// 卫星地图模式
+        //初始化地图控制器对象
+        if (aMap == null) {
+            aMap =  mMapView.getMap();
+        }
+        // 显示卫星地图模式,可选类型：MAP_TYPE_NORMAL,MAP_TYPE_SATELLITE,MAP_TYPE_NIGHT
+        aMap.setMapType(AMap.MAP_TYPE_SATELLITE);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mMapView.onPause();
+    }
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mMapView.onSaveInstanceState(outState);
     }
 
 }
